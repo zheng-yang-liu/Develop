aa37f0288b8a89068510799d1a0ad30d03c33a12
thexu <thexuyan@outlook.com>
2023-03-11 19:42:14 +0800

    git push -u origin master

--- demo2/src/views/jsmk.vue
+++ demo2/src/views/jsmk.vue
@@ -4,6 +4,7 @@
     </div>
 </template>
 <script>
+    //单文件入
     // import date from "@/utils/date.js"
 
     // main.js文件进行引入
@@ -12,14 +13,16 @@
 
     export default {
         mounted() {
-            console.log(this.$date.day());
+            
             // console.log(date.day());
             console.log(get());
             console.log('全局选择导入');
+
+            console.log(this.$date.day());
             console.log(this.$num());
             // 查看全部的路由信息
             console.log(this.$router);
-            // 获取当前访问页面的路由信息
+            //获取当前访问页面的路由信息
             console.log(this.$route);
             
         }

644fabfd1ca1b3bad006f46ac31094d6dc159c65
thexu <thexuyan@outlook.com>
2023-03-31 15:11:45 +0800

    scss练习

--- demo6/src/router/index.js
+++ demo6/src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'index',
-    component:()=> import("../views/index.vue")
+    component:()=> import("../views/sass.vue")
   },
   {
     path: '/news',
---
+++ demo6/src/views/sass.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div>
+            <div class="father">
+                <div class="father-child-one">
+                    father-child-one
+                    <div class="father-child-child-one">father-child-child-one</div>
+                    <div class="father-child-child-two">father-child-child-two</div>
+                </div>
+                    
+                <div class="father-child-two">
+                    father-child-one
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+    $color:red;
+    .father{
+        border: 2px solid $color;
+        width: 50%;
+        div{
+            width: 50%;
+            min-height: 100px;
+            margin: 20px;
+            border: 2px solid green;
+            
+        }
+        .father-child-child-two{
+            color: aqua;
+        }
+        .father-child-one{
+            color:rgb(239, 136, 10);
+            // div:last-child {
+            //     border: 3px solid red;
+            // }
+            div:last-child {
+                border: 3px solid red; 
+            }
+        }
+    }
+</style>

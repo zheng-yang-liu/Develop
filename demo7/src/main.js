71b866438a0890e776c3b5591d50679310866bb5
thexu <thexuyan@outlook.com>
2023-04-26 08:10:22 +0800

    vue链接MySQL数据库

--- demo7/src/main.js
+++ demo7/src/main.js
@@ -3,12 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// 加载 element 组件库
+//加载 element 组件库
 import ElementUI from 'element-ui'
-// 加载 element 组件库的样式
+//加载 element 组件库的样式
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
-
 
 Vue.config.productionTip = false
 

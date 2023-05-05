d3792f119bd23d57cba047a7a8c0da0cc8aa3f16
thexu <thexuyan@outlook.com>
2023-05-05 16:12:19 +0800

    Vuex状态管理

--- demo6/src/store/index.js
+++ demo6/src/store/index.js
@@ -11,16 +11,42 @@ export default new Vuex.Store({
     age:10,
     userinfo:{
       name: '王五',
-      age:20
+      age:20,
+      userID:1
     }
 
   },
   getters: {
     // 针对数据进行二次计算
+    // 名称(state){
+      // return 显示的值
+    // }
+    userID(state){
+      return state.userinfo.userID+1000
+    },
+    // 传入的值来选择传出的值
+    setAge(state){
+      return (num)=>{
+        let newage = state.age+num;
+          return newage
+      }
+    }
+
+
   },
+  // $store.commit(tape函数名称,改变的值)
+  // 变量的修改只能应用以该页面刷新便会原样
   mutations: {
     // 操作数据 state
     // 同步操作
+    // 用来改变state的值
+    // 名称(state,传参(可有可无)){
+      // 函数体
+    // }
+    setName(state,name){
+      state.name = name
+    }
+
   },
   actions: {
     // 操作数据 state
--- demo6/src/views/vuex.vue
+++ demo6/src/views/vuex.vue
@@ -1,36 +1,45 @@
 <template>
     <div>
-        <div>
-            这是Vuex的一个实例
-        </div>
-        <div>全局使用</div>
-        <div>{{$store.state.name}}</div>
-        <div>{{$store.state.userinfo.name}}</div>
-        <div></div>
-        <div>第三种方法</div>
-        <div>{{userinfo.name}}</div>
-        <div>{{userinfo.age}}</div>
+        <h1>Vuex状态管理 </h1>
+        <div>全局调用:{{$store.state.name}}</div>
+        <div>局部调用:{{name}}、{{userinfo.age}}</div>
+
+        <h2>getters</h2>
+        <div>全局调用:{{$store.getters.userID}}</div>
+        <div>局部调用:{{userID}}</div>
+        <h3>带参数</h3>
+        <div>全局调用:{{$store.getters.setAge(10)}}</div>
+        <div>局部调用:{{setAge(10)}}</div>
 
     </div>
 
 </template>
 
 <script>
-    import {mapState} from "vuex"
+    import {mapState,mapGetters} from "vuex"
     export default {
         data(){
             return{
                 
             }
         },
+        created(){
+            console.log("使用生命周期函数调用state"+this.$store.state.name);
+            console.log("使用生命周期函数调用getters"+this.$store.getters.userID);
+            console.log("返回函数题经行返回值"+this.$store.getters.setAge(10));
+            // $store.commit(tape函数名称,改变的值)
+            this.$store.commit('setName','李四')
+        },
+        // 自定义函数位置
+        methods:{
+            
+        },
         computed:{//计算属性
             //拓展运算符
-            ...mapState(['name', 'userinfo'])
-
+            ...mapState(['name', 'userinfo']),
+            ...mapGetters(['userID','setAge'])
         },
-        created(){
-            console.log("第二种方法"+this.$store.state.name);
-        }
+        
     }
 </script>
 

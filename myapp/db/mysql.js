ce0dbedc7738c70400e3450dbb1489d8771b8b78
thexu <thexuyan@outlook.com>
2023-06-01 11:08:48 +0800

    mysql后端学习

---
+++ myapp/db/mysql.js
@@ -0,0 +1,11 @@
+// 引入mysql
+var mysql = require('mysql')
+
+// 链接数据库
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'mysql6zy13172',
+  database: 'node',
+})
+module.exports = connection;
---
+++ myapp/routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+//引入MySQL
+var connection = require('../db/mysql');
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  // 编写sql语句
+  var sql = 'select * from user';
+  // 执行MySQL语句
+  connection.query(sql, function (error, result) { 
+    if (error == null) {
+      res.json({ code: 200, msg: '获取成功', data: result })
+    } else {
+      res.json(error)
+    }
+  })
+ 
+});
+
+module.exports = router;
